18f2ed0046a96c572d6f6110e1692a1736442085
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-11-05 12:54:41 -0700

    Initial Commit

---
+++ SlideMenuDemo/SlideMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuDemoApp.swift
+//  SlideMenuDemo
+//
+//  Created by Vadiraj Hippargi on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
